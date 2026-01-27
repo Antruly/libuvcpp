5724f74880d14b7f6788aab9c7b74d5234788753
unknown <zwy976843568@gmail.com>
2026-01-28 03:04:36 +0800

    fs: guard deep-copy allocations with try/catch and return UV_ENOMEM on failure

--- src/req/uvcpp_fs.cpp
+++ src/req/uvcpp_fs.cpp
@@ -280,21 +280,38 @@ int uvcpp_fs::write(uvcpp_loop* loop,
     _tmp_nbufs = 0;
   }
   _tmp_nbufs = nbufs;
-  _tmp_bufs = new ::uv_buf_t[nbufs];
-  if (_tmp_bases) {
-    for (unsigned int i = 0; i < _tmp_nbufs; ++i) {
-      if (_tmp_bases[i]) uvcpp::uv_free_bytes(_tmp_bases[i]);
+  bool alloc_ok = false;
+  try {
+    _tmp_bufs = new ::uv_buf_t[nbufs];
+    if (_tmp_bases) {
+      for (unsigned int i = 0; i < _tmp_nbufs; ++i) {
+        if (_tmp_bases[i]) uvcpp::uv_free_bytes(_tmp_bases[i]);
+      }
+      delete[] _tmp_bases;
+      _tmp_bases = nullptr;
     }
-    delete[] _tmp_bases;
-    _tmp_bases = nullptr;
+    _tmp_bases = new char*[nbufs];
+    for (unsigned int i = 0; i < nbufs; ++i) {
+      _tmp_bases[i] = (char*)uvcpp::uv_alloc_bytes(bufs[i].len);
+      memcpy(_tmp_bases[i], bufs[i].base, bufs[i].len);
+      _tmp_bufs[i].base = _tmp_bases[i];
+      _tmp_bufs[i].len = bufs[i].len;
+    }
+    alloc_ok = true;
+  } catch (const std::bad_alloc& e) {
+    // cleanup partial allocations
+    if (_tmp_bufs) { delete[] _tmp_bufs; _tmp_bufs = nullptr; }
+    if (_tmp_bases) {
+      for (unsigned int i = 0; i < nbufs; ++i) {
+        if (_tmp_bases[i]) uvcpp::uv_free_bytes(_tmp_bases[i]);
+      }
+      delete[] _tmp_bases;
+      _tmp_bases = nullptr;
+    }
+    _tmp_nbufs = 0;
+    return UV_ENOMEM;
   }
-  _tmp_bases = new char*[nbufs];
-  for (unsigned int i = 0; i < nbufs; ++i) {
-    _tmp_bases[i] = (char*)uvcpp::uv_alloc_bytes(bufs[i].len);
-    memcpy(_tmp_bases[i], bufs[i].base, bufs[i].len);
-    _tmp_bufs[i].base = _tmp_bases[i];
-    _tmp_bufs[i].len = bufs[i].len;
-  }
+  if (!alloc_ok) return UV_ENOMEM;
   return uv_fs_write(OBJ_UVCPP_LOOP_HANDLE(*loop), UVCPP_FS_REQ, file, _tmp_bufs, nbufs, offset, callback_write);
 }
 #if UV_VERSION_MAJOR >= 1
